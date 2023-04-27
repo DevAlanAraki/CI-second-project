e5e2173dc6a2d32ca92e5d5641fc3bb90f4e0b93
Alan Tioshi Araki <alantioshiaraki@gmail.com>
2023-04-27 19:49:08 +0000

    Start creation of the script JS adding the questionary, options and answer

---
+++ assets/js/script.js
@@ -0,0 +1,99 @@
+const quizData = [
+    {
+        question: "How many noses does a slug have?",
+        a: "One",
+        b: "Two",
+        c: "Three",
+        d: "Four",
+        correct: "d"
+    },
+    {
+        question: "What is the largest planet in our solar system?What is the smallest planet in our solar system?",
+        a: "Mercury",
+        b: "Venus",
+        c: "Saturn",
+        d: "Jupiter",
+        correct: "a"
+    },
+    {
+        question: "Which animal can change its color to match its surroundings?",
+        a: "Tiger",
+        b: "Chameleon",
+        c: "Lion",
+        d: "Elephant",
+        correct: "b"
+    },
+    {
+        question: "What is the tallest animal in the world?",
+        a: "Lion",
+        b: "Elephant",
+        c: "Giraffe",
+        d: "Gorilla",
+        correct: "c"
+    },
+    {
+        question: "What is the capital city of Australia?",
+        a: "Sydney",
+        b: "Canberra",
+        c: "Melbourne",
+        d: "Brisbane",
+        correct: "b"
+    },
+    {
+        question: "What is the largest mammal in the world?",
+        a: "Blue Whale",
+        b: "African Elephant",
+        c: "Hippopotamus",
+        d: "Polar Bear",
+        correct: "a"
+    },
+    {
+        question: "What is the name of the highest mountain in the world?",
+        a: "Mount Kilimanjaro",
+        b: "Mount Everest",
+        c: "Mount Denali",
+        d: "Mount Fuji",
+        correct: "b"
+    },
+    {
+        question: "Which planet is closest to the Sun?",
+        a: "Venus",
+        b: "Mars",
+        c: "Mercury",
+        d: "Saturn",
+        correct: "c"
+    },
+    {
+        question: "Which type of animal is a penguin?",
+        a: "Mammal",
+        b: "Reptile",
+        c: "Bird",
+        d: "Fish",
+        correct: "c"
+    },
+    {
+        question: "What is the smallest continent in the world?",
+        a: "Asia",
+        b: "Australia",
+        c: "Africa",
+        d: "Europe",
+        correct: "b"
+    },
+    {
+        question: "How many bones are there in the human body?",
+        a: "206",
+        b: "306",
+        c: "106",
+        d: "406",
+        correct: "a"
+    },
+    {
+        question: "Which instrument is often called the "king of instruments"?",
+        a: "Piano",
+        b: "Violin",
+        c: "Guitar",
+        d: "Organ",
+        correct: "d"
+    },
+
+]
